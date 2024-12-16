51d23c13eb4e7a1dc14eab91496fa9a554cec882
Beatrice Sawyer <bsawyer@kyledavidgroup.com>
2024-12-16 14:24:23 -0500

    resolve ssl error on proxy server

--- kdg.boilerplate.client/vite.config.ts
+++ kdg.boilerplate.client/vite.config.ts
@@ -46,6 +46,10 @@ export default defineConfig({
             '^/weatherforecast': {
                 target,
                 secure: false
+            },
+            '^/api/health': {
+                target,
+                secure: false
             }
         },
         port: 5173,
